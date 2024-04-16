d0c677e3a0b697e09459d5e41fa67ca8604646be
Nick Cho <nicholas.cho@hotmail.ca>
2024-04-16 11:25:48 -0400

    Small syntax errors

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,11 @@ int main(int argc, char* argv[]) {
 		input_buffer->printPrompt();
 		input_buffer->readInput(std::cin);
 		std::string ipt = input_buffer->getBuffer();
-		
+
 		if (ipt == "0") {			
 			exit(EXIT_SUCCESS);
 		} else {
-			printf("Unrecognized command '%s'. \n", ipt->getBuffer().c_str());
+			printf("Unrecognized command '%s'. \n", ipt.c_str());
 		}
 	}
 }
