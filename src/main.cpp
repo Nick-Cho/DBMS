a279248618b0b288e9762cc9e72e9149e7067bc8
Nick Cho <nicholas.cho@hotmail.ca>
2024-04-17 10:33:15 -0400

    Implementing meta command functionality, (chose not to abstract to separate file -> might have to change later)

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,19 @@
 
 #include <iostream>
 
+typedef enum {
+	META_COMMAND_SUCCESS,
+	META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+MetaCommandResult execMetaCommand(InputBuffer* input_buffer) {
+	if (input_buffer->getBuffer() == ".exit") {
+		exit(EXIT_SUCCESS);
+	} else {
+		return META_COMMAND_UNRECOGNIZED_COMMAND;
+	}
+}
+
 int main(int argc, char* argv[]) {
 	InputBuffer* input_buffer = InputBuffer::getBufferInstance();
 	while (true) {
@@ -11,7 +24,7 @@ int main(int argc, char* argv[]) {
 		std::string input = input_buffer->getBuffer();
 
 		if (input == ".") {								
-			switch (doMetaCommand(input_buffer)){
+			switch (execMetaCommand(input_buffer)){
 				case (META_COMMAND_SUCCESS):
 					continue;
 				case (META_COMMAND_UNRECOGNIZED_COMMAND):
