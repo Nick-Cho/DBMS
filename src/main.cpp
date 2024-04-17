fcdfca1e58c1dab97bdd73fe2b5675bcd11bb0b0
Nick Cho <nicholas.cho@hotmail.ca>
2024-04-17 10:28:16 -0400

    Fixing main to reflect statement implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include "./lib/inputBuffer/InputBuffer.h"
+#include "./lib/statement/Statement.h"
+
 #include <iostream>
 
 int main(int argc, char* argv[]) {
@@ -6,32 +8,32 @@ int main(int argc, char* argv[]) {
 	while (true) {
 		input_buffer->printPrompt();
 		input_buffer->readInput(std::cin);
-		std::string ipt = input_buffer->getBuffer();
+		std::string input = input_buffer->getBuffer();
 
-		if (ipt == ".") {								
+		if (input == ".") {								
 			switch (doMetaCommand(input_buffer)){
 				case (META_COMMAND_SUCCESS):
 					continue;
 				case (META_COMMAND_UNRECOGNIZED_COMMAND):
-					printf("Unrecognized command '%s'. \n", ipt.c_str());
+					printf("Unrecognized command '%s'. \n", input.c_str());
 					continue;
 			}
 			Statement statement;
-			switch (prepareStatement(input_buffer, &statement)) {
-				case (PREPARE_SUCCESS):
+			switch (statement.prepareStatement(input)) {
+				case (PrepareResult::PREPARE_SUCCESS):
 					break;
-				case (PREPARE_SYNTAX_ERROR):
+				case (PrepareResult::PREPARE_SYNTAX_ERROR):
 					printf("Syntax error. Could not parse statement. \n");
 					continue;
-				case (PREPARE_UNRECOGNIZED_STATEMENT):
-					printf("Unrecognized keyword at start of '%s'. \n", ipt.c_str());
+				case (PrepareResult::PREPARE_UNRECOGNIZED_STATEMENT):
+					printf("Unrecognized keyword at start of '%s'. \n", input.c_str());
 					continue;
 			}
 
-			executeStatement(&statement);
+			statement.executeStatement();
 			printf("Executed. \n");
 		} else {
-			printf("Unrecognized command '%s'. \n", ipt.c_str());
+			printf("Unrecognized command '%s'. \n", input.c_str());
 		}
 	}
 }
